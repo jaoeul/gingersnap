249e7a03ac9266589763f13784347f7f6a549873
Joel Engelcrona <engelcronajoel@gmail.com>
2021-04-25 00:08:00 +0200

    Add uint64_t to uint8_t byte_array[8] conversion functionality

--- src/shared/endianess_converter.c
+++ src/shared/endianess_converter.c
@@ -2,13 +2,18 @@
 #include <stddef.h>
 #include <stdint.h>
 
+// Remove?
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
 #include "endianess_converter.h"
 
 static uint64_t
-lsb_byte_arr_to_u64(uint8_t bytes[], size_t nb)
+lsb_byte_arr_to_u64(uint8_t bytes[], size_t num)
 {
     uint64_t result = 0;
-    for (uint64_t i = 0; i < nb; i++) {
+    for (uint64_t i = 0; i < num; i++) {
         // Needs cast to uint64_t to not overflow when left shifting
         result |= ((uint64_t)bytes[i]) << (8 * i);
     }
@@ -16,18 +21,50 @@ lsb_byte_arr_to_u64(uint8_t bytes[], size_t nb)
 }
 
 static uint64_t
-msb_byte_arr_to_u64(uint8_t bytes[], size_t nb)
+msb_byte_arr_to_u64(uint8_t bytes[], size_t num)
 {
     uint64_t result = 0;
-    for (uint64_t i = 0; i < nb; i++) {
+    for (uint64_t i = 0; i < num; i++) {
         // Needs cast to uint64_t to not overflow when left shifting
-        result |= ((uint64_t)bytes[i]) << (8 * ((nb - i) - 1));
+        result |= ((uint64_t)bytes[i]) << (8 * ((num - i) - 1));
     }
     return result;
 }
 
 uint64_t
-byte_arr_to_u64(uint8_t* bytes, size_t nb, bool endianess)
+byte_arr_to_u64(uint8_t* bytes, size_t num, ENDIANESS endianess)
 {
-    return endianess ? lsb_byte_arr_to_u64(bytes, nb) : msb_byte_arr_to_u64(bytes, nb);
+    return endianess ? lsb_byte_arr_to_u64(bytes, num) : msb_byte_arr_to_u64(bytes, num);
+}
+
+static void
+u64_to_byte_arr_lsb(uint64_t num, uint8_t result_bytes[8])
+{
+    // For all bits in a uint64_t
+    for (uint64_t i = 0; i < 64; i++) {
+        // If the current bit is set
+        if ((num & ((uint64_t)1 << i)) != 0) {
+            // Set the corresponding bit in the result array
+            result_bytes[(uint8_t)floor(i / 8)] |= ((uint8_t)1 << (i % 8));
+        }
+    }
+}
+
+static void
+u64_to_byte_arr_msb(uint64_t num, uint8_t result_bytes[8])
+{
+    // For all bits in a uint64_t
+    for (uint64_t i = 0; i < 64; i++) {
+        // If the current bit is set
+        if ((num & ((uint64_t)1 << i)) != 0) {
+            // Set the corresponding bit in the result array
+            result_bytes[7 - ((uint8_t)floor(i / 8))] |= ((uint8_t)1 << (i % 8));
+        }
+    }
+}
+
+void
+u64_to_byte_arr(uint64_t num, uint8_t result_bytes[8], ENDIANESS endianess)
+{
+    return endianess ? u64_to_byte_arr_lsb(num, result_bytes) : u64_to_byte_arr_msb(num, result_bytes);
 }
--- src/shared/endianess_converter.h
+++ src/shared/endianess_converter.h
@@ -4,7 +4,15 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef enum {
+    LSB = true,
+    MSB = false,
+} ENDIANESS;
+
 uint64_t
-byte_arr_to_u64(uint8_t* bytes, size_t nb, bool endianess);
+byte_arr_to_u64(uint8_t* bytes, size_t nb, ENDIANESS endianess);
+
+void
+u64_to_byte_arr(uint64_t num, uint8_t result_bytes[8], ENDIANESS endianess);
 
 #endif // ENDIANESS_CONVERTER_H
