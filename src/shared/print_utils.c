356d120c1393d7e93ab14a9e64458c283f41a1b8
Joel Engelcrona <engelcronajoel@gmail.com>
2021-05-23 14:01:37 +0200

    Prettify register dump.

--- src/shared/print_utils.c
+++ src/shared/print_utils.c
@@ -1,6 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 #include "print_utils.h"
 #include "../emu/risc_v_emu.h"
@@ -70,7 +73,6 @@ print_emu_memory(risc_v_emu_t* emu)
         const size_t index       = block / 64; // 64 = number of bits in bitmap entry
         const size_t bit         = block % 64;
         const uint64_t shift_bit = 1;
-
         if ((emu->mmu->dirty_state->dirty_bitmaps[index] & (shift_bit << bit)) == 0) {
             printf("NO");
         }
@@ -111,39 +113,70 @@ print_emu_memory_allocated(risc_v_emu_t* emu)
 void
 print_emu_registers(risc_v_emu_t* emu)
 {
-    printf("emu->zero: 0x%lx\n", emu->registers[REG_ZERO]);
-    printf("emu->ra:   0x%lx\n", emu->registers[REG_RA]);
-    printf("emu->sp:   0x%lx\n", emu->registers[REG_SP]);
-    printf("emu->gp:   0x%lx\n", emu->registers[REG_GP]);
-    printf("emu->tp:   0x%lx\n", emu->registers[REG_TP]);
-    printf("emu->t0:   0x%lx\n", emu->registers[REG_T0]);
-    printf("emu->t1:   0x%lx\n", emu->registers[REG_T1]);
-    printf("emu->t2:   0x%lx\n", emu->registers[REG_T2]);
-    printf("emu->fp:   0x%lx\n", emu->registers[REG_FP]);
-    printf("emu->s1:   0x%lx\n", emu->registers[REG_S1]);
-    printf("emu->a0:   0x%lx\n", emu->registers[REG_A0]);
-    printf("emu->a1:   0x%lx\n", emu->registers[REG_A1]);
-    printf("emu->a2:   0x%lx\n", emu->registers[REG_A2]);
-    printf("emu->a3:   0x%lx\n", emu->registers[REG_A3]);
-    printf("emu->a4:   0x%lx\n", emu->registers[REG_A4]);
-    printf("emu->a5:   0x%lx\n", emu->registers[REG_A5]);
-    printf("emu->a6:   0x%lx\n", emu->registers[REG_A6]);
-    printf("emu->a7:   0x%lx\n", emu->registers[REG_A7]);
-    printf("emu->s2:   0x%lx\n", emu->registers[REG_S2]);
-    printf("emu->s3:   0x%lx\n", emu->registers[REG_S3]);
-    printf("emu->s4:   0x%lx\n", emu->registers[REG_S4]);
-    printf("emu->s5:   0x%lx\n", emu->registers[REG_S5]);
-    printf("emu->s6:   0x%lx\n", emu->registers[REG_S6]);
-    printf("emu->s7:   0x%lx\n", emu->registers[REG_S7]);
-    printf("emu->s8:   0x%lx\n", emu->registers[REG_S8]);
-    printf("emu->s9:   0x%lx\n", emu->registers[REG_S9]);
-    printf("emu->s10:  0x%lx\n",emu->registers[REG_S1]);
-    printf("emu->s11:  0x%lx\n",emu->registers[REG_S1]);
-    printf("emu->t3:   0x%lx\n", emu->registers[REG_T3]);
-    printf("emu->t4:   0x%lx\n", emu->registers[REG_T4]);
-    printf("emu->t5:   0x%lx\n", emu->registers[REG_T5]);
-    printf("emu->t6:   0x%lx\n", emu->registers[REG_T6]);
-    printf("emu->pc:   0x%lx\n", emu->registers[REG_PC]);
+    // Get width of terminal window.
+    unsigned short nb_columns = 80; // Default.
+    struct winsize window;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &window);
+    if (window.ws_col > 80) {
+        nb_columns = window.ws_col;
+    }
+
+    // How many register strings fit horizontally?
+    unsigned short max_len_string = 7 /*ZERO 0x*/ + 18 /*0xffffffffffffffff*/;
+    unsigned short nb_fit = nb_columns / max_len_string;
+
+    // Strings to print.
+    char* register_strings[33] = {0};
+    for (int i = 0; i < sizeof(register_strings) / sizeof(register_strings[0]); ++i) {
+        register_strings[i] = calloc(max_len_string, 1);
+    }
+    uint8_t nb_register_strings = sizeof(register_strings) / sizeof(register_strings[0]);
+
+    sprintf(register_strings[0],  "ZERO 0x%-16lx", emu->registers[REG_ZERO]);
+    sprintf(register_strings[1],  "RA   0x%-16lx", emu->registers[REG_RA]);
+    sprintf(register_strings[2],  "SP   0x%-16lx", emu->registers[REG_SP]);
+    sprintf(register_strings[3],  "GP   0x%-16lx", emu->registers[REG_GP]);
+    sprintf(register_strings[4],  "TP   0x%-16lx", emu->registers[REG_TP]);
+    sprintf(register_strings[5],  "T0   0x%-16lx", emu->registers[REG_T0]);
+    sprintf(register_strings[6],  "T1   0x%-16lx", emu->registers[REG_T1]);
+    sprintf(register_strings[7],  "T2   0x%-16lx", emu->registers[REG_T2]);
+    sprintf(register_strings[8],  "FP   0x%-16lx", emu->registers[REG_FP]);
+    sprintf(register_strings[9], "S1   0x%-16lx", emu->registers[REG_S1]);
+    sprintf(register_strings[10], "A0   0x%-16lx", emu->registers[REG_A0]);
+    sprintf(register_strings[11], "A1   0x%-16lx", emu->registers[REG_A1]);
+    sprintf(register_strings[12], "A2   0x%-16lx", emu->registers[REG_A2]);
+    sprintf(register_strings[13], "A3   0x%-16lx", emu->registers[REG_A3]);
+    sprintf(register_strings[14], "A4   0x%-16lx", emu->registers[REG_A4]);
+    sprintf(register_strings[15], "A5   0x%-16lx", emu->registers[REG_A5]);
+    sprintf(register_strings[16], "A6   0x%-16lx", emu->registers[REG_A6]);
+    sprintf(register_strings[17], "A7   0x%-16lx", emu->registers[REG_A7]);
+    sprintf(register_strings[18], "S2   0x%-16lx", emu->registers[REG_S2]);
+    sprintf(register_strings[19], "S3   0x%-16lx", emu->registers[REG_S3]);
+    sprintf(register_strings[20], "S4   0x%-16lx", emu->registers[REG_S4]);
+    sprintf(register_strings[21], "S5   0x%-16lx", emu->registers[REG_S5]);
+    sprintf(register_strings[22], "S6   0x%-16lx", emu->registers[REG_S6]);
+    sprintf(register_strings[23], "S7   0x%-16lx", emu->registers[REG_S7]);
+    sprintf(register_strings[24], "S8   0x%-16lx", emu->registers[REG_S8]);
+    sprintf(register_strings[25], "S9   0x%-16lx", emu->registers[REG_S9]);
+    sprintf(register_strings[26], "S10  0x%-16lx", emu->registers[REG_S1]);
+    sprintf(register_strings[27], "S11  0x%-16lx", emu->registers[REG_S1]);
+    sprintf(register_strings[28], "T3   0x%-16lx", emu->registers[REG_T3]);
+    sprintf(register_strings[29], "T4   0x%-16lx", emu->registers[REG_T4]);
+    sprintf(register_strings[30], "T5   0x%-16lx", emu->registers[REG_T5]);
+    sprintf(register_strings[31], "T6   0x%-16lx", emu->registers[REG_T6]);
+    sprintf(register_strings[32], "PC   1x%-16lx", emu->registers[REG_PC]);
+
+    // Pretty print.
+    char legend[nb_columns];
+    memset(legend, (uint8_t)'=', nb_columns);
+    printf("%s\n", legend);
+    for (unsigned short i = 0; i < nb_register_strings;) {
+        for (unsigned short j = 0; j < nb_fit && i < nb_register_strings; j++) {
+            printf("%s", register_strings[i++]);
+        }
+        printf("\n");
+    }
+    printf("%s\n", legend);
 }
 
 void
