4f23f113ee78c5cf15809cf6e05371b63e23c66a
Joel Engelcrona <engelcronajoel@gmail.com>
2021-07-09 17:13:10 +0200

    Add single step functionality.

--- src/main/main.c
+++ src/main/main.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
 #include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "../emu/risc_v_emu.h"
 #include "../shared/elf_loader.h"
@@ -7,6 +9,76 @@
 #include "../shared/endianess_converter.h"
 #include "../shared/logger.h"
 #include "../shared/print_utils.h"
+
+#define MAX_DEBUG_COMMAND_LEN 64
+
+// Give the user the ability to show values in the emulator memory, print
+// emulator register state or go to the next instruction. If no debug command
+// was entered, we execute the last entered command, like GDB.
+__attribute__((used))
+    static void
+debug_emu(risc_v_emu_t* emu)
+{
+    static char last_command[MAX_DEBUG_COMMAND_LEN];
+
+    for (;;) {
+        // Get input from the user.
+        char input_buf[MAX_DEBUG_COMMAND_LEN];
+        memset(input_buf, 0, MAX_DEBUG_COMMAND_LEN);
+        fgets(input_buf, MAX_DEBUG_COMMAND_LEN, stdin);
+
+        // We got no new command, resue the last one.
+        if (input_buf[0] == '\n') {
+            memcpy(input_buf, last_command, MAX_DEBUG_COMMAND_LEN);
+        }
+        else {
+            memcpy(last_command, input_buf, MAX_DEBUG_COMMAND_LEN);
+        }
+
+        // New command is memory command, and last one was not.
+        if (strstr(input_buf, "m")) {
+            memcpy(last_command, input_buf, MAX_DEBUG_COMMAND_LEN);
+
+            // Get addresses to show from user input.
+            printf("Address: ");
+            char adr_buf[MAX_DEBUG_COMMAND_LEN];
+            memset(adr_buf, 0, MAX_DEBUG_COMMAND_LEN);
+            fgets(adr_buf, MAX_DEBUG_COMMAND_LEN, stdin);
+            const size_t mem_address = strtoul(adr_buf, NULL, 10);
+
+            // Get addresses to show from user input.
+            printf("Range: ");
+            char range_buf[MAX_DEBUG_COMMAND_LEN];
+            memset(range_buf, 0, MAX_DEBUG_COMMAND_LEN);
+            fgets(range_buf, MAX_DEBUG_COMMAND_LEN, stdin);
+            const size_t mem_range = strtoul(range_buf, NULL, 10);
+
+            print_emu_memory(emu, mem_address, mem_range);
+        }
+
+        // Show emulator register state.
+        if (strstr(input_buf, "r")) {
+            print_emu_registers(emu);
+        }
+
+        // Execute next instruction.
+        if (strstr(input_buf, "ni")) {
+            break;
+        }
+    }
+}
+
+static void
+run_emu(risc_v_emu_t* emu)
+{
+    for (;;) {
+
+#ifdef EMU_MODE_DEBUG
+        debug_emu(emu);
+#endif
+        emu->execute(emu);
+    }
+}
 
 int
 main(int argc, char** argv)
@@ -19,7 +91,7 @@ main(int argc, char** argv)
     // Load the elf into emulator memory.
     // Virtual addresses in the elf program headers corresponds directly to
     // addresses in the emulators memory.
-    load_elf("./src/tests/target", emu);
+    load_elf("./target", emu);
 
     // Create a stack which starts at the current_allocation address of the emulator
     uint64_t stack = emu->mmu->allocate(emu->mmu, stack_size);
@@ -60,11 +132,7 @@ main(int argc, char** argv)
 
     ginger_log(INFO, "Current allocation address: 0x%lx\n", emu->mmu->current_allocation);
 
-    // Run the emulator
-    for (;;) {
-        emu->execute(emu);
-        print_emu_registers(emu);
-    }
+    run_emu(emu);
 
     ginger_log(INFO, "Destroying emu structs!\n");
     emu->destroy(emu);
