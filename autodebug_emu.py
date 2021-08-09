a0563145285282908dd1e7a7cf91a94744f71c0d
Joel Engelcrona <engelcronajoel@gmail.com>
2021-08-09 23:46:11 +0200

    Bug fix

--- autodebug_emu.py
+++ autodebug_emu.py
@@ -17,7 +17,7 @@ import os
 # The registers we care about. Could omit the stack pointer(sp) here.
 cmp_regs = [ "ra", "sp", "gp", "tp", "t0", "t1", "t2", "fp", "s1", "a0", "a1",
              "a2", "a3", "a4", "a5", "a6", "a7", "s2", "s3", "s4", "s5", "s6",
-             "s7", "s8", "s9", "s1", "s1", "t3", "t4", "t5", "t6", "pc"]
+             "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6", "pc"]
 
 def kill_proc(proc_name):
     proc = Popen(['ps', '-A'], stdout=PIPE)
