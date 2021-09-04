f867d547d4b1cce47b535d19db3f53ce1037842f
Joel Engelcrona <engelcronajoel@gmail.com>
2021-09-04 21:23:30 +0200

    Add `target` class for storing information about the target executable.

---
+++ src/target/target.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "target.h"
+
+target_t*
+target_create(const int argc, const heap_str_t argv[])
+{
+    target_t* target = calloc(1, sizeof(target_t));
+    target->argc     = argc;
+    target->argv     = calloc(argc, sizeof(heap_str_t));
+
+    for (int i = 0; i < argc; i++) {
+        memcpy(&target->argv[i], &argv[i], sizeof(argv[i]));
+    }
+
+    // Parse the provided program name as an elf.
+    target->elf = parse_elf(target->argv[0].str);
+
+    return target;
+}
+
+void
+target_destroy(target_t* target)
+{
+    for (int i = 0; i < target->argc; i++) {
+        if (target->argv[i].str) {
+            free(target->argv[i].str);
+        }
+        free(target->argv);
+    }
+    free(target);
+}
---
+++ src/target/target.h
@@ -0,0 +1,19 @@
+#ifndef TARGET_H
+#define TARGET_H
+
+#include "../shared/elf_loader.h"
+#include "../shared/heap_str.h"
+
+typedef struct {
+    int         argc;
+    heap_str_t* argv;
+    elf_t*      elf;
+} target_t;
+
+target_t*
+target_create(const int argc, const heap_str_t argv[]);
+
+void
+target_destroy(target_t* target);
+
+#endif
